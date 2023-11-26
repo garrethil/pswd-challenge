89f075999d0f01f1ee79ca4623e699a4c5e58e73
garrethil <garrethildebrandt@gmail.com>
2023-11-26 13:35:27 -0500

    set arrays and prompts for pswd info

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,15 @@
 // Assignment code here
+var alphabet = ["abcdefghijklmnopqrstuvwxyz"]
+var numbers = ["0123456789"]
+var special = ["!#$%&()*+,-./:;<=>?@^_|~"]
 
+var generatePassword = function(){
+  var pswdLength = window.prompt("How long would you like your password to be? (min. 8 characters)")
+  var lowerCase = window.prompt("Would you like to include lowercase letters? 'Y' or 'N'")
+  var upperCase = window.prompt("Would you like to include uppercase letters? 'Y' or 'N'")
+  var specialChar = window.prompt("Would you like to include special characters? 'Y' or 'N'")
+  var numbs = window.prompt("Would you like to include numbers? 'Y' or 'N'")
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
