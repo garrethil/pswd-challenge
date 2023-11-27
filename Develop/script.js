24acebdddcddc2a61e91a38dbdea1a8cfc103307
garrethil <garrethildebrandt@gmail.com>
2023-11-27 18:26:38 -0500

    checking if character type was selected and add them to a string

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,45 @@
 // Assignment code here
-var alphabet = ["abcdefghijklmnopqrstuvwxyz"]
-var numbers = ["0123456789"]
-var special = ["!#$%&()*+,-./:;<=>?@^_|~"]
 
-var generatePassword = function(){
-  var pswdLength = window.prompt("How long would you like your password to be? (min. 8 characters)")
-  var lowerCase = window.prompt("Would you like to include lowercase letters? 'Y' or 'N'")
-  var upperCase = window.prompt("Would you like to include uppercase letters? 'Y' or 'N'")
-  var specialChar = window.prompt("Would you like to include special characters? 'Y' or 'N'")
-  var numbs = window.prompt("Would you like to include numbers? 'Y' or 'N'")
+
+function generatePassword(){
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+  const alphabetCap = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const numbers = "0123456789";
+  const special = "!#$%&()*+-:;<=>?@^_|~";
+
+  let selections = '';
+
+  var pswdLength = window.prompt("How many characters would you like your password to have? (minimum 8)");
+
+  var lowerCase = window.confirm("Would you like to include lowercase letters?");
+if (lowerCase === true){
+  selections += alphabet
+}
+  var upperCase = window.confirm("Would you like to include uppercase letters?");
+if (upperCase === true){
+  selections += alphabetCap
+}
+  var specialChar = window.confirm("Would you like to include special characters?");
+  if (specialChar === true){
+    selections += special
+  }
+  var numbs = window.confirm("Would you like to include numbers?");
+  if (lowerCase === true){
+    selections += numbers
+  }
+
+  console.log(selections)
+
+
+  
+for (let i = 0; i<pswdLength; i++){
+  var randomIndex = Math.floor(Math.random()* selections.length)
+  password += selections[randomIndex]
+}
+  
+
+
+ return;
 }
 
 // Get references to the #generate element
@@ -16,7 +47,11 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+
+var password = generatePassword();
+
+  
+
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
